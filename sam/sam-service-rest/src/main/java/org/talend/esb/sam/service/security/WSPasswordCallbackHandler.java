18af2743727364336cb6e53ac89326ac7e7cf138
aosadchiy <alexander.g.osadchiy@gmail.com>
2013-05-07 11:47:11 +0300

    add WSPasswordCallbackHandler

--- sam/sam-service-rest/src/main/java/org/talend/esb/sam/service/security/WSPasswordCallbackHandler.java
+++ sam/sam-service-rest/src/main/java/org/talend/esb/sam/service/security/WSPasswordCallbackHandler.java
@@ -1,5 +1,33 @@
 package org.talend.esb.sam.service.security;
 
-public class WSPasswordCallbackHandler {
+import java.io.IOException;
+
+import javax.security.auth.callback.Callback;
+import javax.security.auth.callback.CallbackHandler;
+import javax.security.auth.callback.UnsupportedCallbackException;
+
+import org.apache.ws.security.WSPasswordCallback;
+
+public class WSPasswordCallbackHandler implements CallbackHandler {
+    private final String username;
+    private final String password;
+
+    public WSPasswordCallbackHandler(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    @Override
+    public void handle(Callback[] callbacks) throws IOException, UnsupportedCallbackException {
+        for (Callback callback : callbacks) {
+            if (callback instanceof WSPasswordCallback) {
+                WSPasswordCallback pc = (WSPasswordCallback) callback;
+                if (username.equals(pc.getIdentifier())) {
+                    pc.setPassword(password);
+                    break;
+                }
+            }
+        }
+    }
 
 }
