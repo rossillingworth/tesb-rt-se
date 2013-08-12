33c156bbad0f7f587c4f0ced2cad2d84044bb368
Pavel Vasilchenko <Pavel.S.Vasilchenko@gmail.com>
2013-08-12 23:14:16 +0300

    Fix license headers in correlation ID example

--- examples/tesb/correlationid-callback/src/main/java/org/talend/esb/examples/CorrelationIDCallbackHandlerImpl.java
+++ examples/tesb/correlationid-callback/src/main/java/org/talend/esb/examples/CorrelationIDCallbackHandlerImpl.java
@@ -1,16 +1,21 @@
 /*
- * ============================================================================
- *
- * Copyright (C) 2011 - 2013 Talend Inc. - www.talend.com
- *
- * This source code is available under agreement available at
- * %InstallDIR%\license.txt
- *
- * You should have received a copy of the agreement
- * along with this program; if not, write to Talend SA
- * 9 rue Pages 92150 Suresnes, France
- *
- * ============================================================================ 
+ * #%L
+ * TESB :: Examples :: CorrelationID Callback Handler
+ * %%
+ * Copyright (C) 2011 - 2012 Talend Inc.
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
  */
 package org.talend.esb.examples;
 
