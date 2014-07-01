c6d53067c03af075c4c923ec7d045f9264923104
Alexander Osadchiy <aosadchiy@talend.com>
2014-07-01 10:51:11 +0300

    [TESB-13146] revert fix

--- locator/src/main/java/org/talend/esb/servicelocator/cxf/internal/DefaultSelectionStrategy.java
+++ locator/src/main/java/org/talend/esb/servicelocator/cxf/internal/DefaultSelectionStrategy.java
@@ -36,8 +36,7 @@ import org.apache.cxf.message.Exchange;
  */
 public class DefaultSelectionStrategy extends LocatorSelectionStrategy implements FailoverStrategy {
 
-    // This field is made static as fix for TESB-13146
-    private static Map<QName, String> primaryAddresses = new HashMap<QName, String>();
+    private Map<QName, String> primaryAddresses = new HashMap<QName, String>();
 
     /* (non-Javadoc)
      * @see org.apache.cxf.clustering.FailoverStrategy#getAlternateAddresses(org.apache.cxf.message.Exchange)
