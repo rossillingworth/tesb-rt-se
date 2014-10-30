4771ece1ae4a585b43e6983ab16b769de0b56166
listephen <jianli@talend.com>
2014-10-30 10:09:07 +0800

    Revert "seperate soap service from sam server"
    This reverts commit 9a0de60864ca5df094e7e12523cd3eaf469bea3b.

--- sam/sam-server/src/main/java/org/talend/esb/sam/server/service/MonitoringWebService.java
+++ sam/sam-server/src/main/java/org/talend/esb/sam/server/service/MonitoringWebService.java
@@ -17,7 +17,7 @@
  * limitations under the License.
  * #L%
  */
-package org.talend.esb.sam.soap.service;
+package org.talend.esb.sam.server.service;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
@@ -42,6 +42,8 @@ public class MonitoringWebService implements MonitoringService {
 
     private org.talend.esb.sam.common.service.MonitoringService monitoringService;
     
+    private String enableSAMCollectorService;
+
     /**
      * Sets the monitoring service.
      *
@@ -50,11 +52,23 @@ public class MonitoringWebService implements MonitoringService {
     public void setMonitoringService(org.talend.esb.sam.common.service.MonitoringService monitoringService) {
         this.monitoringService = monitoringService;
     }
+    /**
+     * Set the enableSoapService.
+     */
+    public void setEnableSAMCollectorService(String enableSAMCollectorService) {
+    	this.enableSAMCollectorService = enableSAMCollectorService;
+    }
 
     /* (non-Javadoc)
      * @see org.talend.esb.sam.monitoringservice.v1.MonitoringService#putEvents(java.util.List)
      */
     public String putEvents(List<EventType> eventTypes) throws PutEventsFault {
+    	//enableSoapService != null && true not equal enableSoapService, only rest service , return
+    	if ((enableSAMCollectorService != null && !"true".equals(enableSAMCollectorService)) 
+    			|| enableSAMCollectorService == null) {
+    		LOG.info("enableSAMCollectorService : " + enableSAMCollectorService + " from sam-server.");
+    		return null;
+    	}
         if (LOG.isLoggable(Level.INFO)) {
             LOG.info("Received event(" + eventTypes.size() + ") from Agent.");
         }
