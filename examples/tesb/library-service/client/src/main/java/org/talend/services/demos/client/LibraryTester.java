55417401a833475597c81928b6f49770cb57a31a
Andrei Shakirin <andrei.shakirin@gmail.com>
2015-01-26 11:14:51 +0100

    Updated LibraryExample callback to be compatible with ASF

--- examples/tesb/library-service/client/src/main/java/org/talend/services/demos/client/LibraryTester.java
+++ examples/tesb/library-service/client/src/main/java/org/talend/services/demos/client/LibraryTester.java
@@ -15,7 +15,6 @@ import org.talend.services.demos.library._1_0.Library;
 import org.talend.services.demos.library._1_0.SeekBookError;
 import org.talend.types.demos.library.common._1.ListOfBooks;
 import org.talend.types.demos.library.common._1.SearchFor;
-import org.talend.types.demos.library.common._1.SearchInBasementFor;
 
 /**
  * The Class LibraryTester.
@@ -159,7 +158,7 @@ public class LibraryTester implements InitializingBean {
         System.out.println("*** Request-Callback operation ********************************");
         System.out.println("***************************************************************");
         System.out.println("\nSending request(callback) for authors named Stripycat");
-        SearchInBasementFor request = new SearchInBasementFor();
+        SearchFor request = new SearchFor();
         request.getAuthorLastName().add("Stripycat");
         Map<String, Object> rctx = ((BindingProvider) libraryJms).getRequestContext();
         Map<String, Object> correlationInfo = new HashMap<String, Object>();
