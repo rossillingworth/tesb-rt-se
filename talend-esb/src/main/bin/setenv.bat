ea4876f76d7d4a51af742f1cb5379628dfb8715c
Igor Lazebny <ilazebny@talend.com>
2015-07-14 21:51:23 +0300

    [TESB-15762] Don't set PermSize options for Java version > 7

--- talend-esb/src/main/bin/setenv.bat
+++ talend-esb/src/main/bin/setenv.bat
@@ -55,13 +55,23 @@ rem SET KARAF_BASE
 rem Additional available Karaf options
 rem SET KARAF_OPTS
 
-rem determine whether we use a 64-bit java version
+rem Find java version we use
 
 set java_version_file=%time::=%
 set /a java_version_file=java_version_file
 set java_version_file=__JVER%java_version_file%%random%.tmp
 "%JAVA%" -version 2> %java_version_file%
+rem Determine whether we use a 64-bit java version
 for /f %%G IN ('findstr "64-Bit" %java_version_file%') DO set sixtyfour=true
+rem Check minor java version number in string like 'java version "1.7.0_80"'
+for /f "tokens=3" %%g in ('findstr /i /c:"java version" %java_version_file%') do (
+    set JAVAVER=%%g
+)
+set JAVAVER=%JAVAVER:"=%
+set JAVAVER_MINOR=7
+for /f "delims=. tokens=2" %%v in ("%JAVAVER%") do (
+    set JAVAVER_MINOR=%%v
+)
 del %java_version_file%
 
 rem Check/Set up some easily accessible MIN/MAX params for JVM mem usage
@@ -82,18 +92,20 @@ if "%JAVA_MAX_MEM%" == "" (
 	)
 )
 
-if "%JAVA_PERM_MEM%" == "" (
-	if "%sixtyfour%" == "" (
-		set JAVA_PERM_MEM=64M 
-	) else (
-		set JAVA_PERM_MEM=128M
-	)
-)
-
-if "%JAVA_MAX_PERM_MEM%" == "" (
-	if "%sixtyfour%" == "" (
-		set JAVA_MAX_PERM_MEM=384M
-	) else (
-		set JAVA_MAX_PERM_MEM=640M
-	)
+if %JAVAVER_MINOR% LSS 8 (
+    if "%JAVA_PERM_MEM%" == "" (
+    	if "%sixtyfour%" == "" (
+    		set JAVA_PERM_MEM=64M 
+    	) else (
+    		set JAVA_PERM_MEM=128M
+    	)
+    )
+    
+    if "%JAVA_MAX_PERM_MEM%" == "" (
+    	if "%sixtyfour%" == "" (
+    		set JAVA_MAX_PERM_MEM=384M
+    	) else (
+    		set JAVA_MAX_PERM_MEM=640M
+    	)
+    )
 )
