a9ec1e939731afe34d8a403c15c826631ddc112a
Colm O hEigeartaigh <coheigea@apache.org>
2015-08-19 14:32:42 +0100

    NPE fix

--- auxiliary-storage/security-rest/src/main/java/org/talend/esb/auxiliary/storage/rest/security/WSPasswordCallbackHandler.java
+++ auxiliary-storage/security-rest/src/main/java/org/talend/esb/auxiliary/storage/rest/security/WSPasswordCallbackHandler.java
@@ -21,9 +21,13 @@ import javax.security.auth.callback.CallbackHandler;
 import javax.security.auth.callback.UnsupportedCallbackException;
 
 import org.apache.wss4j.common.ext.WSPasswordCallback;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class WSPasswordCallbackHandler implements CallbackHandler {
 
+    private static final transient Logger LOG = LoggerFactory.getLogger(WSPasswordCallbackHandler.class);
+                                                                        
     private final String user;
     private final String pass;
 
@@ -34,6 +38,11 @@ public class WSPasswordCallbackHandler implements CallbackHandler {
 
     @Override
     public void handle(Callback[] callbacks) throws IOException, UnsupportedCallbackException {
+        if (user == null) {
+            LOG.debug("No user was specified in the WSPasswordCallbackHandler");
+            return;
+        }
+        
         for (Callback callback : callbacks) {
             if (callback instanceof WSPasswordCallback) {
                 WSPasswordCallback pc = (WSPasswordCallback) callback;
