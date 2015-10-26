b4325502f8fc189a4b2e1c384f652728164a0c17
PavloVasylchenko <Pavel.S.Vasilchenko@gmail.com>
2015-10-26 12:43:01 +0200

    TESB-16366 Implement bundle that helps to encrypt passwords

---
+++ text-encryptor/src/main/java/org/talend/esb/encryptor/TextEncryptor.java
@@ -0,0 +1,43 @@
+package org.talend.esb.encryptor;
+
+import org.apache.karaf.shell.api.action.Action;
+import org.apache.karaf.shell.api.action.Argument;
+import org.apache.karaf.shell.api.action.Command;
+import org.apache.karaf.shell.api.action.lifecycle.Service;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.EnvironmentStringPBEConfig;
+import org.jasypt.properties.PropertyValueEncryptionUtils;
+
+//TODO: Add description
+@Command(scope = "tesb", name = "encrypt-helper", description = "--")
+@Service
+public class TextEncryptor implements Action {
+
+    private static final String ALGORITHM = "PBEWITHSHA256AND128BITAES-CBC-BC";
+    private static final String PASSWORD_ENV_NAME = "TESB_ENV_PASSWORD";
+    private static final String PROVIDER_NAME = "BC";
+
+
+    //TODO: Add description
+    @Argument(index = 0,
+            name = "TextToEncrypt",
+            description = "--",
+            required = true,
+            multiValued = false)
+    String textToEncrypt;
+
+    @Override
+    public Object execute() throws Exception {
+        //TODO: check that system env var is not null
+        StandardPBEStringEncryptor enc = new StandardPBEStringEncryptor();
+        EnvironmentStringPBEConfig env = new EnvironmentStringPBEConfig();
+        env.setProvider(new BouncyCastleProvider());
+        env.setProviderName(PROVIDER_NAME);
+        env.setAlgorithm(ALGORITHM);
+        env.setPasswordEnvName(PASSWORD_ENV_NAME);
+        enc.setConfig(env);
+        System.out.println(PropertyValueEncryptionUtils.encrypt(textToEncrypt, enc));
+        return null;
+    }
+}
