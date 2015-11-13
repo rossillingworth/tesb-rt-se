8c591c6d59f516051e704d789212413496de26c8
Alex Telesh <atelesh@talend.com>
2015-11-13 14:02:37 +0300

    TESB-12296 - tESBConumer should allow to specify the SL Properties when 'Servcie Registry' option is selected

--- locator/src/main/java/org/talend/esb/servicelocator/cxf/LocatorFeature.java
+++ locator/src/main/java/org/talend/esb/servicelocator/cxf/LocatorFeature.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -43,6 +43,8 @@ public class LocatorFeature extends AbstractFeature implements LocatorFeatureInt
     private SLPropertiesMatcher slPropsMatcher;
 
     private String selectionStrategy;
+
+    public static final String LOCATOR_PROPERTIES = "esb.locator.properties";
 
     @Override
     public void initialize(Bus bus) {
