86b678fe23cdca781a40250848f8f226e878fa73
Christian Schneider <chris@die-schneider.net>
2016-01-27 13:35:39 +0100

    Fixes

--- examples/tesb/ebook/ebook-ui/src/main/resources/app.js
+++ examples/tesb/ebook/ebook-ui/src/main/resources/app.js
@@ -1,16 +1,17 @@
 (function() {
     var app = angular.module('ebook', ['ngResource']);
     app.controller('BookController', function($scope, $http, $resource) {
+	var resource = "/cxf/ebook";
         $scope.book = {};
         $scope.idSelectedBook = null;
     	$scope.reload = function() {
-            $http.get("/cxf/ebook")
+            $http.get(resource)
             .success(function (response) {$scope.books = response.book;});
          };
     
          $scope.select = function(bookId) {
              $scope.idSelectedBook = bookId;
-             var Book = $resource('/cxf/ebook/:id', {id:'@id'});
+             var Book = $resource(resource + '/:id', {id:'@id'});
              var book = Book.get({id:bookId}, function() {
                  $scope.book = book.book;
              });
@@ -18,7 +19,7 @@
           };
           
           $scope.send = function(bookId, recipient) {
-        	  var Book = $resource('/cxf/ebook/:id', {id:'@id'});
+              var Book = $resource(resource + '/:id', {id:'@id'});
               var result = Book.post({id:bookId}, function() {
                   alert(result);
               });
