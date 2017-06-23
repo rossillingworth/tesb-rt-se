ff90a05aee62a573f6556db628e552e69f9edbe9
Vladimir Bogatyrov <vbogatyrov@talend.com>
2017-06-23 12:07:14 +0300

    [TESB-19834] cTalendJob uses wrong context parameters

--- camel-talendjob/src/main/java/org/talend/camel/TalendEndpoint.java
+++ camel-talendjob/src/main/java/org/talend/camel/TalendEndpoint.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -43,6 +43,7 @@ public class TalendEndpoint extends DefaultEndpoint {
     private String context;
     private Map<String, String> endpointProperties;
     private boolean stickyJob;
+    private String componentId = null;
 
     private boolean propagateHeader = true;
 
@@ -81,6 +82,14 @@ public class TalendEndpoint extends DefaultEndpoint {
 
     public void setStickyJob(boolean stickyJob) {
         this.stickyJob = stickyJob;
+    }
+
+    public String getComponentId() {
+        return componentId;
+    }
+
+    public void setComponentId(String componentId) {
+        this.componentId = componentId;
     }
 
     public TalendJob getJobInstance() throws Exception {
